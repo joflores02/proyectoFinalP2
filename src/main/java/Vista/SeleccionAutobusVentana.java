b4cd2dc6cd735fcc09f820b4187805a6e555291d
Jocabed Aileen <joclopez2021@inf.udec.cl>
2024-11-29 14:17:20 -0300

    Se crea una ventana vacía para la clase SeleccionAutobusVentana

--- src/main/java/Vista/SeleccionAutobusVentana.java
+++ src/main/java/Vista/SeleccionAutobusVentana.java
@@ -1,2 +1,18 @@
-package Vista;public class SeleccionAutobusVentana {
+package Vista;
+
+import javax.swing.*;
+
+public class SeleccionAutobusVentana {
+
+    public SeleccionAutobusVentana() {
+        JFrame ventana = new JFrame("Selección de Autobús");
+        ventana.setSize(1120, 652);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SeleccionAutobusVentana();
+    }
 }
