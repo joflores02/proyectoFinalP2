063280e8b78eb4105ea5beb9d738cecf24fc28c2
Jocabed Aileen <joclopez2021@inf.udec.cl>
2024-11-29 19:32:56 -0300

    Se crea una ventana para confirmar la reserva

--- src/main/java/Vista/ConfirmarReservaVentana.java
+++ src/main/java/Vista/ConfirmarReservaVentana.java
@@ -1,2 +1,23 @@
-package Vista;public class ConfirmarReservaVentana {
+package Vista;
+
+import javax.swing.*;
+
+public class ConfirmarReservaVentana extends JDialog {
+    public ConfirmarReservaVentana(JFrame parent) {
+        super(parent, "Confirmar Reserva", true);
+        setSize(400, 250);
+        setLocationRelativeTo(parent);
+    }
+
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame();
+        mainFrame.setSize(400, 300);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setLocationRelativeTo(null);
+
+        ConfirmarReservaVentana dialogo = new ConfirmarReservaVentana(mainFrame);
+
+        dialogo.setVisible(true);
+
+    }
 }
