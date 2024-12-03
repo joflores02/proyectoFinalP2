b0a98b518c430905e707de89c1a8277926ebb8cc
Liivne <ignsoto2023@inf.udec.cl>
2024-12-03 01:19:03 -0300

    Horarios y recorrido

--- src/main/java/Modelo/Horario.java
+++ src/main/java/Modelo/Horario.java
@@ -1,18 +1,22 @@
-package Modelo;public class Horario {
+package Modelo;
+
+import java.util.List;
+
+public class Horario {
     //Aquí asignamos día y hora, de partida y llegada de cada bus
     //También, si es que el usuario elige cierto día,
     // deberían aparecer en pantalla todos los buses disponibles ese día (pero eso se ve en interfaz)
 
-    private int dia;
-    private int horas;
-    private int minutos;
+    private String horaLlegada;
+    private String horaSalida;
+    private List<Horario> horarios;
 
+    private String dia;
 
-    public class Dia{}
-    public void Hora(int horas, int minutos){
-        this.minutos = minutos;
+    public Horario(String horaSalida, String horaLlegada) {
+        this.horaSalida = horaSalida;
+        this.horaLlegada = horaLlegada;
     }
-
 
     public void agregarHorario(Horario horario) {
         horarios.add(horario);
