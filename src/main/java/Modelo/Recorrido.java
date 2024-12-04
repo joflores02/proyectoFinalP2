efef4e3e78a690a38ef1efc88a02bafbc8c33128
Jocabed Aileen <joclopez2021@inf.udec.cl>
2024-12-04 03:09:41 -0300

    Contenido de la clase Recorrido

--- src/main/java/Modelo/Recorrido.java
+++ src/main/java/Modelo/Recorrido.java
@@ -1,2 +1,28 @@
-package Modelo;public class Recorrido {
+package Modelo;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recorrido {
+    private String lugarInicio;
+    private String lugarFinal;
+
+
+    public Recorrido(String lugarInicio, String lugarFinal) {
+        this.lugarInicio = lugarInicio;
+        this.lugarFinal = lugarFinal;
+    }
+
+
+    public String getLugarInicio() {
+        return lugarInicio;
+    }
+
+    public String getLugarFinal() {
+        return lugarFinal;
+    }
+
+    @Override
+    public String toString() {
+        return lugarInicio + " - " + lugarFinal;
+    }
 }
